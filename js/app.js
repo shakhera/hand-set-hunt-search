acba1490ffc62c239f2e52734f96dc195b77c4e6
shakhera <shakherakhanom024@gmail.com>
2023-09-04 18:31:44 +0600

    hand set hunt search

--- js/app.js
+++ js/app.js
@@ -34,8 +34,7 @@ const displayPhones = (phones, dataLimit) => {
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.</p>
+                
                 <button onclick="loadPhoneDetails('${phone.slug}')" id="" class="btn btn-primary ms-2" type="button" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Details</button>
                 
                 </div>
